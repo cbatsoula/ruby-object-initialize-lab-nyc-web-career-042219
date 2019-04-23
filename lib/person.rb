dda54d31f1ae35982ace84b3420a3a02a04e9de7
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-23 15:08:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,6 @@
 class Person 
-  def initialize(breed)
-	3.    @breed = breed
-	4.  end
-	5.  
-	6.  def breed=(breed)
-	7.    @breed = breed
-	8.  end
-	9.  
-	10.  def breed
-   @breed
- end
-  
+  def initialize(name)
+	 @name = name
+end
   
 end
